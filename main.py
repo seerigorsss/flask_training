86474e5023b6a65ebba1e349c4c9449398f59c68
Igor Sirotkin <myEmail@example.com>
2023-04-22 15:57:06 +0500

    Решение задачи "Автоматический ответ"

--- main.py
+++ main.py
@@ -14,5 +14,20 @@ def training(prof):
     return render_template('training.html', profession=prof)
 
 
+@app.route('/answer')
+@app.route('/auto_answer')
+def answer():
+    params = {}
+    params['title'] = 'Анкета'
+    params['surname'] = 'Watny'
+    params['name'] = 'Mark'
+    params['education'] = 'выше среднего'
+    params['profession'] = 'штурман марсохода'
+    params['sex'] = 'male'
+    params['motivation'] = 'Всегда мечтал застрять на Марсе'
+    params['ready'] = 'True'
+    return render_template('auto_answer.html', values=params)
+
+
 if __name__ == '__main__':
     app.run(port=5000, host='127.0.0.1')
