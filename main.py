427fc27b565dd78a00da737c9d7cc8ba88da02e8
Igor Sirotkin <myEmail@example.com>
2023-04-22 20:43:25 +0500

    Решение задачи "По каютам!"

--- main.py
+++ main.py
@@ -14,5 +14,10 @@ def training(prof):
     return render_template('training.html', profession=prof)
 
 
+@app.route('/distribution')
+def distribution():
+    return render_template('distribution.html')
+
+
 if __name__ == '__main__':
     app.run(port=5000, host='127.0.0.1')
