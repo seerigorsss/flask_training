f46d5d4efdc665b170f180213c0bbb0fccb6013c
Igor Sirotkin <myEmail@example.com>
2023-04-23 20:24:56 +0500

    Решение задачи "Цвет каюты"

--- main.py
+++ main.py
@@ -14,5 +14,10 @@ def training(prof):
     return render_template('training.html', profession=prof)
 
 
+@app.route('/table/<sex>/<age>')
+def table(sex, age):
+    return render_template('table.html', sex=sex, age=int(age))
+
+
 if __name__ == '__main__':
     app.run(port=5000, host='127.0.0.1')
