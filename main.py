9e5c3ace0aedf192a0a85728ca7ccae5788d0cb7
Igor Sirotkin <myEmail@example.com>
2023-04-24 18:05:08 +0500

    Решение задачи "Галерея с загрузкой"

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+import json
+
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
@@ -12,6 +15,16 @@ def index(title):
 @app.route('/training/<prof>', methods=["GET"])
 def training(prof):
     return render_template('training.html', profession=prof)
+
+
+@app.route('/member')
+def member():
+    with open('templates/members.json', encoding='utf-8') as json_file:
+        f = json_file.read()
+        data = json.loads(f)
+        number = random.randint(0, len(data) - 1)
+        profs = sorted(data[number]["professions"])
+    return render_template('member.html', index=number, json=data, professions=profs)
 
 
 if __name__ == '__main__':
