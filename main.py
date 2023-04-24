e8dd41294d69f3899ac181e9bf89f4933e2f59a9
Igor Sirotkin <myEmail@example.com>
2023-04-24 17:17:52 +0500

    Решение задачи "Галерея с загрузкой"

--- main.py
+++ main.py
@@ -14,5 +14,10 @@ def training(prof):
     return render_template('training.html', profession=prof)
 
 
+@app.route('/gallery', methods=["POST", "GET"])
+def gallery():
+    return render_template('gallery.html')
+
+
 if __name__ == '__main__':
     app.run(port=5000, host='127.0.0.1')
